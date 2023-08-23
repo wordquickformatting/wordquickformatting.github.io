ff5f59c6e954583be9029dd8b69641708844c045
Huu Nguyen <thieuvan0405@gmail.com>
2023-08-23 22:31:45 +0700

    add anim

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     console.log('loaded');
-//     // sticky header on scroll script
-//     if (this.scrollY > 20) {
-//       $('.header').addClass('sticky');
-//     } else {
-//       $('.header').removeClass('sticky');
-//     }
-
-//     // scroll-up button show/hide script
-//     if (this.scrollY > 500) {
-//       $('.scroll-up-btn').addClass('show');
-//     } else {
-//       $('.scroll-up-btn').removeClass('show');
-//     }
-//   });
-
-//   $('.menu-btn').click(function () {
-//     $('header .nav-links').toggleClass('active');
-//     $('.menu-btn i').toggleClass('active');
-//   });
-
-//   $('.link-menu').click(function () {
-//     $('header .nav-links').removeClass('active');
-//     $('.menu-btn i').removeClass('active');
-//   });
-// });
+const header = document.getElementById('header');
 
 window.addEventListener('scroll', () => {
-  const header = document.getElementById('header');
   if (this.scrollY > 20) {
     header.classList.add('sticky');
   } else {
