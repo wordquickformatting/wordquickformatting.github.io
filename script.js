fbdc89d3a5bd4fb91d89e858b3bb013dfaf91e28
Huu Nguyen <thieuvan0405@gmail.com>
2023-10-01 18:28:55 +0700

    add move modal

--- script.js
+++ script.js
@@ -22,21 +22,51 @@ const modal = document.getElementById('modal-feedback');
 const modalImg = document.getElementById('feedback-modal-img');
 
 const imgs = document.getElementsByClassName('feedback-img');
-
-// imgs.forEach(img => {
-//   img.onclick = () => {
-//     modal.style.display = 'block';
-//     modalImg.src = img.src;
-//   };
-// });
-
-function feedbackClick(src) {
-  modal.style.display = 'block';
-  modalImg.src = src;
-}
+let currentImage = 0;
+const moveLeft = document.getElementById('move-left');
+const moveRight = document.getElementById('move-right');
 
 const closeBtn = document.getElementById('close-modal-btn');
 
+function resetMoveDisplay() {
+  moveLeft.style.display = 'unset';
+  moveRight.style.display = 'unset';
+}
+
+function updateMoveDisplay() {
+  if (currentImage <= 1) moveLeft.style.display = 'none';
+  else if (currentImage >= imgs.length) moveRight.style.display = 'none';
+  else {
+    resetMoveDisplay();
+  }
+}
+
 closeBtn.onclick = () => {
   modal.style.display = 'none';
+  resetMoveDisplay();
+};
+
+function feedbackClick(src, index) {
+  modal.style.display = 'block';
+  modalImg.src = src;
+  currentImage = index;
+  updateMoveDisplay();
+}
+
+moveLeft.onclick = function () {
+  currentImage -= 1;
+  updateMoveDisplay();
+  if (currentImage < 1) return;
+  let imgSrc = currentImage;
+  imgSrc = `assets/feedback-${imgSrc}.jpg`;
+  modalImg.src = imgSrc;
+};
+
+moveRight.onclick = function () {
+  currentImage += 1;
+  updateMoveDisplay();
+  if (currentImage > imgs.length) return;
+  let imgSrc = currentImage;
+  imgSrc = `assets/feedback-${imgSrc}.jpg`;
+  modalImg.src = imgSrc;
 };
